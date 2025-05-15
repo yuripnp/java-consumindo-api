f847a1a86be32c2b4636da04158a354020206ac2
yuripnp <yuripnp1992@gmail.com>
2025-05-14 21:05:56 -0300

    melhorando exceção

--- src/desafiofinal/ConsultaCep.java
+++ src/desafiofinal/ConsultaCep.java
@@ -13,16 +13,14 @@ public class ConsultaCep {
 
     public Endereco buscaEndereco(String cep) {
         URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
-        HttpClient cliente = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(endereco).build();
-
-        HttpResponse<String> response = null;
         try {
-            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
             throw new RuntimeException("Não consegui obter o endereço apartir do cep, error: " + e);
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
+
     }
 }
